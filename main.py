78b4eeb5f8ced86ce5032e31efdd0871bf04cab6
tanzimozer <154046621+tanzimozer@users.noreply.github.com>
2024-12-15 13:27:38 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("python scripts/data_collection.py")
+os.system("python scripts/feature_engineering.py")
+os.system("python scripts/exploratory_data_analysis.py")
+os.system("python scripts/predictive_modeling.py")
+os.system("python scripts/visualization_and_reporting.py")
